4c6c4a37037c071a97660fc32853a7937c6eb700
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2024-09-09 15:03:45 +0300

    Fix date format

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -10,5 +10,5 @@ export function formatMoney(cents: number): string {
 }
 
 export function formatDate(date: Date): string {
-    return date.getFullYear() + "-" + date.getMonth().toString().padStart(2, "0") + "-" + date.getDay().toString().padStart(2, "0") + " " + date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0") + ":" + date.getSeconds().toString().padStart(2, "0");
+    return date.getFullYear() + "-" + (date.getMonth()+1).toString().padStart(2, "0") + "-" + date.getDate().toString().padStart(2, "0") + " " + date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0") + ":" + date.getSeconds().toString().padStart(2, "0");
 }
